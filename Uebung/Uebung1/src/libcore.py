c9ed393241f720be169f7307b2001df100b6164b
Stefano Di Martino <stefano.d@posteo.de>
2016-03-30 18:23:36 +0200

    loaded image is already ndarray

--- Uebung/Uebung1/src/libcore.py
+++ Uebung/Uebung1/src/libcore.py
@@ -4,8 +4,8 @@ import numpy as np
 class Img:
     @staticmethod
     def load_image(path, as_grey = False, to_float = True):
-        # Load image and convert to numpy array
-        image = np.array(imread(path, as_grey))
+        # Load image
+        image = imread(path, as_grey)
 
         if to_float:
             # Convert to floating point matrix
