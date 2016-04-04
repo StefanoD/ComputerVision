261693f461f82e0483982876defda3e334616173
Stefano Di Martino <stefano.d@posteo.de>
2016-04-04 12:13:29 +0200

    aufgabe 1 a refactoring

--- Uebung/Uebung1/src/exercise_a.py
+++ Uebung/Uebung1/src/exercise_a.py
@@ -7,33 +7,6 @@ from libcore import Img
 
 from libcore import RestructuringMethod
 
-
-def test_indirekte_Umbildung(image, ):
-
-
-
-    # More than 2 dimensions
-    #if len(image.shape) > 2:
-    #    new_dim = (image.shape[0] * 1.5, image.shape[1] * 1.5, len(image.shape))
-    #else:
-    new_dim = (image.shape[0] * 1.5, image.shape[1] * 1.5)
-
-    new_image = np.zeros(new_dim)
-    new_image = new_image.astype(np.float32)
-
-
-
-
-    # back casting to uint8
-    uint8_array = image.astype(np.uint8)
-    #print uint8_array
-    # show picture
-    plt.imshow(uint8_array)
-    plt.show()
-
-
-
-
 def indirect_restructuring(image,
                            transform_matrix,
                            translation_vector,
@@ -44,8 +17,6 @@ def indirect_restructuring(image,
     #    new_dim = (image.shape[0] * 1.5, image.shape[1] * 1.5, len(image.shape))
     #else:
 
-    print image.shape
-
     new_dim = (image.shape[0] * 1.5, image.shape[1] * 1.5, 3)
 
     new_image = np.zeros(new_dim)
@@ -54,30 +25,29 @@ def indirect_restructuring(image,
     x_size = new_image.shape[0]
     y_size = new_image.shape[1]
 
-    for row in range(x_size):
-        for col in range(y_size):
-            new_row = row - translation_vector[0]
-            new_col = col - translation_vector[1]
+    for x in range(x_size):
+        for y in range(y_size):
+            new_coordinates = np.array([x, y])
+            new_coordinates = new_coordinates - translation_vector
 
-            tmp = np.array([new_row, new_col])
+            new_x = new_coordinates[0]
+            new_y = new_coordinates[1]
 
-
-            if new_row < 0 or new_col < 0 or row >= image.shape[0] or col >= image.shape[1]:
-                new_image[row, col] = 0
+            if new_x < 0 or new_y < 0 or x > image.shape[0] or y > image.shape[1]:
+                new_image[x, y] = 0
             else:
-                new_image[row, col] = image[new_row, new_col, 0]
-                new_image[row, col] = image[new_row, new_col, 1]
-                new_image[row, col] = image[new_row, new_col, 2]
+                new_image[x, y, 0] = image[new_x, new_y, 0]
+                new_image[x, y, 1] = image[new_x, new_y, 1]
+                new_image[x, y, 2] = image[new_x, new_y, 2]
 
             #print(row, col)
             #print(new_row, new_col)
 
 
 
-
     # back casting to uint8
     uint8_array = new_image.astype(np.uint8)
-    #print uint8_array
+
     # show picture
     plt.imshow(uint8_array)
     plt.show()
