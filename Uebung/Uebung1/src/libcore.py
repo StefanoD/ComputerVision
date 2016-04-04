ef44ff0181e6571284033e5507d5aee09ab29caa
Stefano Di Martino <stefano.d@posteo.de>
2016-04-04 09:49:52 +0200

    drehmatrix

--- Uebung/Uebung1/src/libcore.py
+++ Uebung/Uebung1/src/libcore.py
@@ -1,5 +1,7 @@
 from skimage.data import imread
 import numpy as np
+from enum import Enum
+import math
 
 class Img:
 
@@ -14,9 +16,41 @@ class Img:
 
         return image
 
+    @staticmethod
+    def get_2d_rotation_matrix(degrees):
+        rotation_matrix = np.empty((2, 2))
+
+        rotation_matrix[0, 0] = math.cos(degrees)
+        rotation_matrix[0, 1] = math.sin(degrees)
+        rotation_matrix[1, 0] = -math.sin(degrees)
+        rotation_matrix[1, 1] = math.cos(degrees)
+
+        return rotation_matrix
+
+    @staticmethod
+    def get_x_3d_rotation_matrix(degrees):
+        """Rotation through x axis"""
+
+        rotation_matrix = np.empty((3, 3))
+
+        rotation_matrix[0, 0, 0] = 1
+
+        rotation_matrix[1, 1, 1] = math.cos(degrees)
+        rotation_matrix[1, 1, 2] = -math.sin(degrees)
+
+        rotation_matrix[2, 2, 1] = math.sin(degrees)
+        rotation_matrix[2, 2, 2] = math.cos(degrees)
+
+        return rotation_matrix
+
 
 class Transform:
 
     @staticmethod
     def translate(matrix, trans_vector):
         return matrix + trans_vector
+
+
+class RestructuringMethod(Enum):
+    NearestNeighbor = 1
+    BilinearInterpolation = 2
--- Uebung/Uebung1/src/main.py
+++ Uebung/Uebung1/src/main.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
-import importlib
+import numpy as np
+import exercise_a
+from libcore import Img
 
-# import libcore this way for PyCharm
-libcore = importlib.import_module('libcore')
-Img = libcore.Img
 
 def main():
     img = Img.load_image('../gletscher.jpg')
 
+    rotation_matrix = Img.get_2d_rotation_matrix(30)
+    exercise_a.indirect_restructuring(img, rotation_matrix, [], [])
 
-    print(img)
 
 
 if __name__ == "__main__": main()
