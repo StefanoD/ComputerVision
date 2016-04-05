40f9373ebd86de6861cd75f3ee41b51ca349b665
Stefano Di Martino <stefano.d@posteo.de>
2016-04-06 00:11:25 +0200

    bilinear_interpolation prototyp

--- Uebung/Uebung1/src/exercise_a.py
+++ Uebung/Uebung1/src/exercise_a.py
@@ -38,15 +38,18 @@ def indirect_restructuring(image,
             new_x = new_coordinates[0]
             new_y = new_coordinates[1]
 
-            if restructuring_method == RestructuringMethod.BilinearInterpolation:
-                pass
-            else:
+            if restructuring_method == RestructuringMethod.NearestNeighbor:
                 new_x, new_y = nearest_neighboor(new_x, new_y)
 
             if new_x > 0 and new_y > 0 and new_x < image.shape[0] and new_y < image.shape[1]:
-                new_image[x, y, 0] = image[new_x, new_y, 0]
-                new_image[x, y, 1] = image[new_x, new_y, 1]
-                new_image[x, y, 2] = image[new_x, new_y, 2]
+                if restructuring_method == RestructuringMethod.BilinearInterpolation:
+                    new_image[x, y, 0] = bilinear_interpolation(image[:, :, 0], new_x, new_y)
+                    new_image[x, y, 1] = bilinear_interpolation(image[:, :, 1], new_x, new_y)
+                    new_image[x, y, 2] = bilinear_interpolation(image[:, :, 2], new_x, new_y)
+                else:
+                    new_image[x, y, 0] = image[new_x, new_y, 0]
+                    new_image[x, y, 1] = image[new_x, new_y, 1]
+                    new_image[x, y, 2] = image[new_x, new_y, 2]
 
 
     # back casting to uint8
@@ -55,6 +58,32 @@ def indirect_restructuring(image,
     # show picture
     plt.imshow(uint8_array)
     plt.show()
+
+def bilinear_interpolation(image, x, y):
+    # TODO Index Error Out of bounds. Ungetestet!
+    x_left = int(x)
+    x_right = int(x + 1)
+
+    y_upper = int(y)
+    y_lower = int(y + 1)
+
+    # calculate areas
+    a1 = (x - x_left) * (y - y_upper)
+    a2 = (x_right - x) * (y - y_upper)
+    a3 = (x - x_left) * (y_lower - y)
+    a4 = (x_right - x) * (y_lower - y)
+
+    grey_value_left_upper = image[x_left, y_upper]
+    grey_value_right_upper = image[x_right, y_upper]
+
+    grey_value_left_lower = image[x_left, y_lower]
+    grey_value_right_lower = image[x_right, y_lower]
+
+    bilinear_interpolated_gray_value = grey_value_left_upper * a4 + grey_value_right_upper * a3 + \
+                                       grey_value_left_lower * a2 + grey_value_right_lower * a1
+
+    return bilinear_interpolated_gray_value
+
 
 def nearest_neighboor(x, y):
     # round coordinates
