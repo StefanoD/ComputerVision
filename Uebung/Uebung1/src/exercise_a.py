cd062ce9b8781f2311b1287a0d5a9041f2dcb295
Stefano Di Martino <stefano.d@posteo.de>
2016-04-09 18:29:16 +0200

    Moving restructuring methods to libcore

--- Uebung/Uebung1/src/exercise_a.py
+++ Uebung/Uebung1/src/exercise_a.py
@@ -4,7 +4,6 @@ from skimage.data import imread
 import matplotlib.pyplot as plt
 
 from libcore import Img
-
 from libcore import RestructuringMethod
 
 def indirect_restructuring(image,
@@ -39,13 +38,13 @@ def indirect_restructuring(image,
             new_y = new_coordinates[1]
 
             if restructuring_method == RestructuringMethod.NearestNeighbor:
-                new_x, new_y = nearest_neighboor(new_x, new_y)
+                new_x, new_y = RestructuringMethod.nearest_neighboor(new_x, new_y)
 
             if new_x > 0 and new_y > 0 and new_x < image.shape[0] and new_y < image.shape[1]:
                 if restructuring_method == RestructuringMethod.BilinearInterpolation:
-                    new_image[x, y, 0] = bilinear_interpolation(image[:, :, 0], new_x, new_y)
-                    new_image[x, y, 1] = bilinear_interpolation(image[:, :, 1], new_x, new_y)
-                    new_image[x, y, 2] = bilinear_interpolation(image[:, :, 2], new_x, new_y)
+                    new_image[x, y, 0] = RestructuringMethod.bilinear_interpolation(image[:, :, 0], new_x, new_y)
+                    new_image[x, y, 1] = RestructuringMethod.bilinear_interpolation(image[:, :, 1], new_x, new_y)
+                    new_image[x, y, 2] = RestructuringMethod.bilinear_interpolation(image[:, :, 2], new_x, new_y)
                 else:
                     new_image[x, y, 0] = image[new_x, new_y, 0]
                     new_image[x, y, 1] = image[new_x, new_y, 1]
@@ -58,43 +57,3 @@ def indirect_restructuring(image,
     # show picture
     plt.imshow(uint8_array)
     plt.show()
-
-def bilinear_interpolation(image, x, y):
-    x_left = int(x)
-    x_right = int(x + 1)
-
-    y_upper = int(y)
-    y_lower = int(y + 1)
-
-    # Because we added 1 on x and y, we could possibly be over
-    # the range of the image
-    image_x_max_index = image.shape[0] - 1
-    image_y_max_index = image.shape[1] - 1
-
-    if (x_right > image_x_max_index or y_lower > image_y_max_index):
-        return image[x, y]
-
-    # calculate areas
-    a1 = (x - x_left) * (y - y_upper)
-    a2 = (x_right - x) * (y - y_upper)
-    a3 = (x - x_left) * (y_lower - y)
-    a4 = (x_right - x) * (y_lower - y)
-
-    grey_value_left_upper = image[x_left, y_upper]
-    grey_value_right_upper = image[x_right, y_upper]
-
-    grey_value_left_lower = image[x_left, y_lower]
-    grey_value_right_lower = image[x_right, y_lower]
-
-    bilinear_interpolated_gray_value = grey_value_left_upper * a4 + grey_value_right_upper * a3 + \
-                                       grey_value_left_lower * a2 + grey_value_right_lower * a1
-
-    return bilinear_interpolated_gray_value
-
-
-def nearest_neighboor(x, y):
-    # round coordinates
-    new_x = int(x + 0.5)
-    new_y = int(y + 0.5)
-
-    return new_x, new_y
--- Uebung/Uebung1/src/libcore.py
+++ Uebung/Uebung1/src/libcore.py
@@ -45,13 +45,59 @@ class Img:
         return rotation_matrix
 
 
+
 class Transform:
 
     @staticmethod
     def translate(matrix, trans_vector):
         return matrix + trans_vector
 
+    @staticmethod
+    def degree_to_rad(degree):
+        return degree * math.pi / 180.0
+
 
 class RestructuringMethod(Enum):
     NearestNeighbor = 1
     BilinearInterpolation = 2
+
+    @staticmethod
+    def bilinear_interpolation(image, x, y):
+        x_left = int(x)
+        x_right = int(x + 1)
+
+        y_upper = int(y)
+        y_lower = int(y + 1)
+
+        # Because we added 1 on x and y, we could possibly be over
+        # the range of the image
+        image_x_max_index = image.shape[0] - 1
+        image_y_max_index = image.shape[1] - 1
+
+        if (x_right > image_x_max_index or y_lower > image_y_max_index):
+            return image[x, y]
+
+        # calculate areas
+        a1 = (x - x_left) * (y - y_upper)
+        a2 = (x_right - x) * (y - y_upper)
+        a3 = (x - x_left) * (y_lower - y)
+        a4 = (x_right - x) * (y_lower - y)
+
+        grey_value_left_upper = image[x_left, y_upper]
+        grey_value_right_upper = image[x_right, y_upper]
+
+        grey_value_left_lower = image[x_left, y_lower]
+        grey_value_right_lower = image[x_right, y_lower]
+
+        bilinear_interpolated_gray_value = grey_value_left_upper * a4 + grey_value_right_upper * a3 + \
+                                           grey_value_left_lower * a2 + grey_value_right_lower * a1
+
+        return bilinear_interpolated_gray_value
+
+    @staticmethod
+    def nearest_neighboor(x, y):
+        # round coordinates
+        new_x = int(x + 0.5)
+        new_y = int(y + 0.5)
+
+        return new_x, new_y
