d4a31bc3e32f66e3d116d2e12e2700ac426a6dee
anhaegel <anhaegel@htwg-konstanz.de>
2016-04-25 09:30:26 +0200

    Aufgabenblatt2 fertig

--- Uebung/Uebung2/src/exercise2.py
+++ Uebung/Uebung2/src/exercise2.py
@@ -9,7 +9,6 @@ image_path = '../schraegbild_tempelhof.jpg'
 
 def main():
     b1()
-    pass
 
 
 def b1():
@@ -18,8 +17,8 @@ def b1():
     target_image_size_height = 900
     target_image_size_witdh = 600
 
-    new_x_size = int(image.shape[0] * 3)
-    new_y_size = int(image.shape[1] * 3)
+    new_x_size = target_image_size_witdh*3
+    new_y_size = target_image_size_height*3
 
     new_image = np.zeros((new_x_size, new_y_size, 3))
 
@@ -35,9 +34,11 @@ def b1():
     target_point_2_x = 0.0
     target_point_2_y = target_image_size_height
 
+    pass_point_3_x = 694.0
+    pass_point_3_y = 432.0
     #rechts unten
-    pass_point_3_x = 690.0
-    pass_point_3_y = 460.0
+    #pass_point_3_x = 690.0
+    #pass_point_3_y = 460.0
     target_point_3_x = target_image_size_witdh
     target_point_3_y = target_image_size_height
 
@@ -75,26 +76,16 @@ def b1():
     c2 = a[7]
 
 
-    for x_old in np.arange(0, image.shape[0]):
-        for y_old in np.arange(0, image.shape[1]):
+    for x_old in np.arange(0, image.shape[1]):
+        for y_old in np.arange(0, image.shape[0]):
+            denominator = (b1*c2 - b2*c1)*y_old + (a2*c1 - a1*c2)*x_old + a1*b2 - a2*b1
+            new_x = ((b2 - c2*b3)*y_old + (a3*c2 - a2)*x_old + a2*b3 - a3*b2)/denominator
+            new_y = ((b3*c1 - b1)*y_old + (a1 - a3*c1)*x_old + a3*b1 - a1*b3)/denominator
 
-            #x_new = x_old + pass_point_1_x
-            #y_new = y_old + pass_point_1_y
-
-            x_new = x_old
-            y_new = y_old
-
-            denominator = (b1*c2 - b2*c1)*y_new + (a2*c1 - a1*c2)*x_new + a1*b2 - a2*b1
-            new_x = ((b2 - c2*b3)*y_new + (a3*c2 - a2)*x_new + a2*b3 - a3*b2)/denominator
-            new_y = ((b3*c1 - b1)*y_new + (a1 - a3*c1)*x_new + a3*b1 - a1*b3)/denominator
-
-
-
-            if new_x > 0 and new_y > 0 and new_x < image.shape[0] and new_y < image.shape[1]:
-
-                new_image[y_new, x_new, 0] = image[new_x, new_y, 0]
-                new_image[y_new, x_new, 1] = image[new_x, new_y, 1]
-                new_image[y_new, x_new, 2] = image[new_x, new_y, 2]
+            if new_x > 0 and new_y > 0 and new_x < image.shape[1] and new_y < image.shape[0]:
+                new_image[x_old, y_old, 0] = RestructuringMethod.bilinear_interpolation(image[:, :, 0], new_y, new_x)
+                new_image[x_old, y_old, 1] = RestructuringMethod.bilinear_interpolation(image[:, :, 1], new_y, new_x)
+                new_image[x_old, y_old, 2] = RestructuringMethod.bilinear_interpolation(image[:, :, 2], new_y, new_x)
 
     imsave("../images/test.jpg", new_image)
 
